b0f92f1974fe08955c205b4e82bea300296c4c75
Marcus <mdavis73@aacc.edu>
2021-02-27 20:17:07 -0500

    Initial Commit

---
+++ Coffee/CoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeApp.swift
+//  Coffee
+//
+//  Created by Marcus Davis on 2/27/21.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
